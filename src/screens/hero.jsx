e163ffcbd78bb3a843efb3614e0fa669acd11523
Aditya <itsadityasharma7124@gmail.com>
2023-10-16 18:16:56 +0530

    modified hero sec and few changes

--- src/screens/hero.jsx
+++ src/screens/hero.jsx
@@ -3,11 +3,17 @@ import Navbar from '../components/navbar'
 import Taxi from "../images/yellowTaxi.svg"
 import '../styles/hero.css'
 
-export default function hero() {
+export default function Hero() {
+
+  const timings = "Mon-Sun: 5:00 AM To 11:00PM";
   return (
     <div className='hero'>
       <Navbar />
       <img src={Taxi} className='hero-taxi-img' alt="hero" />
+      <div className='timings'>
+        <i style={{ marginRight: "6px" }} class="fa-solid fa-clock" ></i>
+        {timings}
+      </div>
       <div className='tagline'>
         <p style={{ display: "flex", flexDirection: "row-reverse" }}>Moving People</p>
         and the <strong style={{ color: "#FFC440" }}>Ahmedabad</strong>
@@ -15,10 +21,10 @@ export default function hero() {
           <div className='bookNow-heading'>Book Now</div>
           <div className='bookNow-icons'>
             <div className='bookNow-icon'>
-              <i class="fa-solid fa-phone-volume" style={{transform: "scale(0.5)",}}></i>
+              <i class="fa-solid fa-phone-volume" style={{ transform: "scale(0.5)", }}></i>
             </div>
             <div className='bookNow-icon'>
-              <i class="fa-brands fa-whatsapp" style={{transform: "scale(0.67)"}}></i>
+              <i class="fa-brands fa-whatsapp" style={{ transform: "scale(0.67)" }}></i>
             </div>
           </div>
         </div>
